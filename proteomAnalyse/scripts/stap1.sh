0b984f216c45f877bae45122de1d0e11434633de
alenagrrr3 <asava388@gmail.com>
2025-01-22 15:57:07 +0100

    add raw data proteomic analyse

---
+++ proteomAnalyse/scripts/stap1.sh
@@ -0,0 +1,4 @@
+gth -genomic sequence.fasta  -protein  idmapping_2025_01_18.fasta -xmlout  > output.chr1.lumrube.xml
+
+# this wil produce xml alginment file, see map alignemts
+# raw data, fasta's :  raw_data/idmapping_2025_01_18.fasta
