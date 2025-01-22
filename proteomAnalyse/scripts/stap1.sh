8f7d29867fcb8ac34a35091ed0f13e04d5defdf0
alenagrrr3 <asava388@gmail.com>
2025-01-22 16:55:52 +0100

    add proteoeme analyse scripts

--- proteomAnalyse/scripts/stap1.sh
+++ proteomAnalyse/scripts/stap1.sh
@@ -1,3 +1,8 @@
+
+# pre-requirements
+#conda install bioconda::genomethreader
+
+
 gth -genomic sequence.fasta  -protein  idmapping_2025_01_18.fasta -xmlout  > output.chr1.lumrube.xml
 
 # this wil produce xml alginment file, see map alignemts
