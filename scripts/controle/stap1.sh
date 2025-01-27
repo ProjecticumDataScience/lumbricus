9c4b255d0b2ff0cf6c868e370ae25da7d00906fc
alenagrrr3 <asava388@gmail.com>
2025-01-27 20:09:38 +0100

    add c elegans controle

---
+++ scripts/controle/stap1.sh
@@ -0,0 +1,32 @@
+
+# resultaten van de pipline positieve controle C Elegans hier, tracks+refseq: https://wclumterr.netlify.app/
+# Unprot proteome:
+
+
+#  Step 1: proteome Uniprot alignment 
+startAlign.pl --genome BX284601.5.fasta --prot UP000001940_6239.fasta  --pos OX457036.1:1-10000000  --prg gth
+
+# object concat.aln to gtf
+
+gth2gtf.pl gth.concat.aln  bonafide.gtf
+
+# gtf to bonafide
+computeFlankingRegion.pl bonafide.gtf
+
+gff2gbSmallDNA.pl bonafide.gtf  BX284601.5.fasta  1450 bonafide.gb
+
+
+# create new species celegans 
+ new_species.pl --species=celegans
+
+etraining --species=celegans  bonafide.gb &> bonafide.out
+
+
+randomSplit.pl bonafide.gb  200
+
+
+
+etraining --species=celegans  train.gb &> etrain.out
+
+
+augustus --species=celgans  test.gb > test.out
