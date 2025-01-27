f5e59bbb3f73609ca283cd89723508d3bd24b378
alenagrrr3 <asava388@gmail.com>
2025-01-27 20:45:05 +0100

    add gene database

--- scripts/protocol2/stap1.sh
+++ scripts/protocol2/stap1.sh
@@ -5,7 +5,7 @@
 # prerequirements: 1.  sudo cp -r regenworm /usr/share/augustus/config/species/
 #  2.  cp -r regenworm  anaconda3/envs/c/config/species/
 # model:  https://github.com/ProjecticumDataScience/lumbricus/tree/master/data/protocol2/model
-# raw  fasta's :  raw_data/idmapping_2025_01_18.fasta
+# raw  https://raw.githubusercontent.com/ProjecticumDataScience/lumbricus/refs/heads/master/data_input/protocol02/proteins/idmapping_2025_01_18.fasta
 
 fastas=("chr1.fasta" "chr2.fasta" "chr3.fasta" "chr4.fasta")
 
@@ -21,4 +21,5 @@ done
 #gth -genomic sequence.fasta  -protein  idmapping_2025_01_18.fasta -xmlout  > output.chr1.lumrube.xml
 
 # this wil produce xml alginment file, this xml alignment is used in the second step : 
-#https://github.com/ProjecticumDataScience/lumbricus/blob/master/scripts/protocol2/stap2.sh
+
+#protocol2/stap2.sh
--- scripts/protocol2/stap2.sh
+++ scripts/protocol2/stap2.sh
@@ -3,3 +3,11 @@
 # usage :
 
 python dbscript.py -i inputfile.xlm  -o output.txt
+
+
+# output file contains gene database all identified genes:
+# Lumbricus Terrestris:
+#https://github.com/ProjecticumDataScience/lumbricus/blob/master/results/gen_database/gen_database_lumterr.txt
+
+# Lumbricus Rubellus:
+# https://github.com/ProjecticumDataScience/lumbricus/blob/master/results/gen_database/gen_database_lurube.txt
