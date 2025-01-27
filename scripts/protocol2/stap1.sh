58bd667525d18c829a29fbb428c8aeda0129fecf
alenagrrr3 <asava388@gmail.com>
2025-01-27 20:39:11 +0100

    add proteins protocol2

--- scripts/protocol2/stap1.sh
+++ scripts/protocol2/stap1.sh
@@ -4,7 +4,7 @@
 
 # prerequirements: 1.  sudo cp -r regenworm /usr/share/augustus/config/species/
 #  2.  cp -r regenworm  anaconda3/envs/c/config/species/
-# model:  https://github.com/ProjecticumDataScience/lumbricus/tree/master/data/protocol2/model/regenworm
+# model:  https://github.com/ProjecticumDataScience/lumbricus/tree/master/data/protocol2/model
 # raw  fasta's :  raw_data/idmapping_2025_01_18.fasta
 
 fastas=("chr1.fasta" "chr2.fasta" "chr3.fasta" "chr4.fasta")
