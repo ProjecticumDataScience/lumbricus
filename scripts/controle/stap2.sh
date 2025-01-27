b47ffef50552729a79639113d534012b4f7c0804
alenagrrr3 <asava388@gmail.com>
2025-01-27 20:24:06 +0100

    add proteins id mapping fasta

--- scripts/controle/stap2.sh
+++ scripts/controle/stap2.sh
@@ -1,7 +1,8 @@
 # this step wil produce new species celegans
-# model:
-#
-
+# test-model:
+#https://github.com/ProjecticumDataScience/lumbricus/tree/master/data/controle/celegans/test-model/celegans
+# visualization of test-model:
+#  https://wclumterr.netlify.app/
 
 # gtf to bonafide
 computeFlankingRegion.pl bonafide.gtf
@@ -15,9 +16,11 @@ gff2gbSmallDNA.pl bonafide.gtf  BX284601.5.fasta  1450 bonafide.gb
 etraining --species=celegans  bonafide.gb &> bonafide.out
 
 
-randomSplit.pl bonafide.gb  200
 
 
+randomSplit.pl bonafide.gb 200
+mv bonafide.gb.test test.gb
+mv bonafide.gb.train train.gb
 
 etraining --species=celegans  train.gb &> etrain.out
 
