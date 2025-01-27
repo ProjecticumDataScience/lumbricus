65c5b1dc368ce13bf3f5bc2e2caacae184b626ad
alenagrrr3 <asava388@gmail.com>
2025-01-27 20:35:44 +0100

    add model protocol2

--- scripts/protocol1/step8.sh
+++ scripts/protocol1/step8.sh
@@ -1,5 +1,6 @@
-#  this scripts prepares bonafide to build a new species
-# before we build a new spies we have to format a string in gb file:
+# *** this step is needed only if your compiler doesn't identifies the Unique LOCUS in Bonafide.gf ***
+# 
+# If there are no uniqe identfiers, we need to format a string in a gb file:
 # LOCUS       OX457036.1 Lumbricus terrestris genome assembly, chromosome: 1_18743774-18745105   1332 bp  DNA
 # this string  contains unique ID after the comma, and compiler can not access it. We have to split this string in order
 # to give access to the unique ID ( 1_18743774-18745105).
@@ -7,8 +8,8 @@
 
 # in python script:
 
-#Je moet het "pattern" aanpassen naar je bonafide file: 
-#dit is een string die na de LOCUS komt, zoals deze
+#You need to change the “pattern” according to your bonafide  file: 
+#this is a string that comes after the LOCUS, like this one
 # " OX457036.1 Lumbricus terrestris genoomassemblage, chromosoom: 1_50559-55310."
 
 # 
