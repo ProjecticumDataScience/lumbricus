da749e6878074e60f857b25cece98050009860a4
alenagrrr3 <asava388@gmail.com>
2025-01-28 23:24:26 +0100

    update p2

--- scripts/protocol2.1/stap2.sh
+++ scripts/protocol2.1/stap2.sh
@@ -11,22 +11,6 @@ startAlign.pl --genome Lumbricus_terrestris-GCA_949752735.1-softmasked.fa  --pro
 # Als je fout "line 673" ziet, is dat niets anders dan een geheugenfout. Het wordt opgelost  door de fasta te splitsen
 # in 4 segmenten en een startAlign.pl  uit te voeren in een lus
 
-# Dit is de cyclus
-
-posities=(“wcL:1-22500000”  ” wcL:22500000-45000000”  “wcL:45000000- 67500000”  “wcL:67500000- 90000000”  )
-
-for pos in ${posities[@]}; do
-    	echo $pos
-             	startAlign.pl --genome=genome.fa  --prot=proteome.fasta --prg=gth pos=$pos
-
-done
-
-
-# een fasta-bestand aangepast aan deze lus hier :
-https://genome321.s3.amazonaws.com/genome.fa
-# (dit is dezelfde fasta Lumbricus_terrestris-GCA_949752735.1- , maar met aangepaste headercoördinaten, 
-# om het mogelijk te maken zo'n cyclus te doorlopen).
-# resulterende files is 4 text files gth.concat.aln, die je samen kan mergen
 
 ## Alternatief: Je kunt een fasta-bestand splitsen in twee delen met faSplit en vervolgens een alignment voor elk deel uitvoeren
 ## vervolgens 2 text bestanden mergen
