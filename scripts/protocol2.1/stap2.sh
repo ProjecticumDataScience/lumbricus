bd7c26028a658e83faed3dd19343e44889d5cd6d
alenagrrr3 <asava388@gmail.com>
2025-01-28 01:49:08 +0100

    add scripts protocol2.1

--- scripts/protocol2.1/stap2.sh
+++ scripts/protocol2.1/stap2.sh
@@ -1,4 +1,7 @@
 # this script creates object concat align, which is needed to create model
 
+startAlign.pl --genome Lumbricus_terrestris-GCA_949752735.1-softmasked.fa  --prot proteome.fa  --prg  gth  
 
+# proteome  :https://github.com/ProjecticumDataScience/lumbricus/tree/master/data/protocol2.1/uniprot_proteome
 
+# Lumbricus_terrestris-GCA_949752735.1-softmasked.fa: https://ftp.ensembl.org/pub/rapid-release/species/Lumbricus_terrestris/GCA_949752735.1/ensembl/genome/
