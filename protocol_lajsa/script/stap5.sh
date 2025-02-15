8d0b8ef3be7a3d268756be909ecaff4eca969f11
alenagrrr3 <asava388@gmail.com>
2025-02-15 17:09:49 +0100

    rm p12

--- protocol_lajsa/script/stap5.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#deze script is voor van wig naar hints voor augustus
-
-cat rnaseq.wig | wig2hints.pl --width=10 --margin=10 --minthresh=2 \
---minscore=4 --prune=0.1 --src=W --type=ep \
---UCSC=unstranded.track --radius=4.5 --pri=4 --strand="." \
-> rnaseq.gff
